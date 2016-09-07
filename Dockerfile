b7d0301b00868641b9efa8573a57fa13b74313a3
Christoph Jansen <jansen.christoph@yahoo.de>
2016-09-07 12:03:36 +0200

    build from specific CC-Image-Ubuntu version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM docker.io/curiouscontainers/cc-image-ubuntu
+FROM docker.io/curiouscontainers/cc-image-ubuntu:0.1
 COPY config.toml /opt/config.toml
 
 COPY algorithm.sh /home/ubuntu/algorithm.sh
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 REGISTRY_URL=docker.io/curiouscontainers/cc-sample-app
 
-docker pull docker.io/curiouscontainers/cc-image-ubuntu
+docker pull docker.io/curiouscontainers/cc-image-ubuntu:0.1
 docker pull ${REGISTRY_URL}
 docker build -t ${REGISTRY_URL} .
 docker push ${REGISTRY_URL}

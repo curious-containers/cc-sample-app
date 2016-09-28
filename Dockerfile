cbcef357e7d735975522405ca2ec858a62498298
Christoph Jansen <jansen.christoph@yahoo.de>
2016-09-28 17:57:26 +0200

    version 0.2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM docker.io/curiouscontainers/cc-image-ubuntu:0.1
+FROM docker.io/curiouscontainers/cc-image-ubuntu:0.2
 COPY config.toml /opt/config.toml
 
 COPY algorithm.sh /home/ubuntu/algorithm.sh
--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 REGISTRY_URL=docker.io/curiouscontainers/cc-sample-app
+TAG=0.2
 
-docker pull docker.io/curiouscontainers/cc-image-ubuntu:0.1
+docker pull docker.io/curiouscontainers/cc-image-ubuntu:${TAG}
 docker pull ${REGISTRY_URL}
 docker build -t ${REGISTRY_URL} .
 docker push ${REGISTRY_URL}
+
+if [[ ! -z ${TAG} ]]; then
+    docker tag ${REGISTRY_URL} ${REGISTRY_URL}:${TAG}
+    docker push ${REGISTRY_URL}:${TAG}
+fi

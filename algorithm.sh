2e45e2033edb8d0978c3cadaecbb4515d10643d9
Christoph Jansen <jansen.christoph@yahoo.de>
2016-10-17 14:30:10 +0200

    optional flag

--- algorithm.sh
+++ algorithm.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-cp /home/ubuntu/input_files/data.txt /home/ubuntu/result_files/data.txt
+if [[ $(($RANDOM % 2)) == 0 ]]; then
+    cp /home/ubuntu/input_files/data.txt /home/ubuntu/result_files/data.txt
+fi
 echo ${@} > /home/ubuntu/result_files/parameters.txt

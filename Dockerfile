4a305b79a8c99b4e1d683a6a00896bad4e17db86
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2017-02-10 17:17:33 +0100

    version 0.10

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM docker.io/curiouscontainers/cc-image-ubuntu:0.9
+FROM docker.io/curiouscontainers/cc-image-ubuntu:0.10
 COPY config.json /opt/config.json
 
 COPY algorithm.sh /home/ubuntu/algorithm.sh
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 REGISTRY_URL=docker.io/curiouscontainers/cc-sample-app
-TAG=0.9
+TAG=0.10
 
 docker pull docker.io/curiouscontainers/cc-image-ubuntu:${TAG}
 docker pull ${REGISTRY_URL}
